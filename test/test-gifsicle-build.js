6f851ca1d7210fa532ac43ed1098d8a371c1ec7a
Kevin Martensson <kevinmartensson@gmail.com>
2013-07-31 02:17:23 +0200

    Add request-progress to deps

--- test/test-gifsicle-build.js
+++ test/test-gifsicle-build.js
@@ -12,7 +12,7 @@ describe('Gifsicle rebuild', function () {
 		this.timeout(false);
 
 		var origCTime = fs.statSync(binPath).ctime;
-		exec('node lib/check.js runBuild', {}, function (err) {
+		exec('node lib/check.js', {}, function (err) {
 			var actualCTime = fs.statSync(binPath).ctime;
 			assert(actualCTime !== origCTime);
 			cb(err);
