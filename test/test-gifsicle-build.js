b13cd3be727513b52855eddb3e3534da2254a246
Kevin Martensson <kevinmartensson@gmail.com>
2013-07-31 13:25:00 +0200

    Fetch and extract inside `util.build()`

--- test/test-gifsicle-build.js
+++ test/test-gifsicle-build.js
@@ -2,9 +2,12 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
 var exec = require('child_process').exec;
 var assert = require('assert');
 var binPath = require('../lib/gifsicle-bin').path;
+var srcUrl = require('../lib/gifsicle-bin').src;
+var util = require('../lib/util');
 
 describe('Gifsicle rebuild', function () {
 	it('it should rebuild the gifsicle binaries', function (cb) {
@@ -12,7 +15,7 @@ describe('Gifsicle rebuild', function () {
 		this.timeout(false);
 
 		var origCTime = fs.statSync(binPath).ctime;
-		exec('node lib/check.js', {}, function (err) {
+		util.build(srcUrl, path.dirname(binPath), function (err) {
 			var actualCTime = fs.statSync(binPath).ctime;
 			assert(actualCTime !== origCTime);
 			cb(err);
