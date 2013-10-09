26b1bd4f0c23ba2456565fafba6762a2fa09143e
Kevin Martensson <kevinmartensson@gmail.com>
2013-10-09 10:18:55 +0200

    `bin.path` should always return local binary in tests

--- test/test-build.js
+++ test/test-build.js
@@ -10,12 +10,12 @@ var path = require('path');
 describe('gifsicle.build()', function () {
 	it('should rebuild the gifsicle binaries', function (cb) {
 		this.timeout(false);
-
-		options.path = path.join(__dirname, '../tmp');
-		options.buildScript = './configure --disable-gifview --disable-gifdiff --bindir="' + path.join(__dirname, '../tmp') +
-							  '" && make install';
-
 		var bin = new Bin(options);
+
+		bin.path = path.join(__dirname, '../tmp', bin.bin);
+		bin.buildScript = './configure --disable-gifview --disable-gifdiff ' +
+				 		  '--bindir=' + path.join(__dirname, '../tmp') + ' && ' +
+				 		  'make install';
 
 		bin.build(function () {
 			var origCTime = fs.statSync(bin.path).ctime;
