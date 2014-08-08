0498ce93d2643b6251165364d08af78eee80b1a2
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-08 16:18:35 +0200

    Minor improvements

--- index.js
+++ index.js
@@ -2,22 +2,30 @@
 
 var BinBuild = require('bin-build');
 var BinWrapper = require('bin-wrapper');
-var chalk = require('chalk');
 var fs = require('fs');
+var logSymbols = require('log-symbols');
 var path = require('path');
+var pkg = require('./package.json');
+
+/**
+ * Variables
+ */
+
+var BIN_VERSION = '1.83';
+var BASE_URL = 'https://raw.github.com/imagemin/gifsicle-bin/v' + pkg.version + '/vendor/';
 
 /**
  * Initialize a new BinWrapper
  */
 
 var bin = new BinWrapper()
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/osx/gifsicle', 'darwin')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/linux/x86/gifsicle', 'linux', 'x86')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/linux/x64/gifsicle', 'linux', 'x64')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/freebsd/x86/gifsicle', 'freebsd', 'x86')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/freebsd/x64/gifsicle', 'freebsd', 'x64')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/win/x86/gifsicle.exe', 'win32', 'x86')
-	.src('https://raw.github.com/imagemin/gifsicle-bin/0.1.6/vendor/win/x86/gifsicle.exe', 'win32', 'x64')
+	.src(BASE_URL + 'osx/gifsicle', 'darwin')
+	.src(BASE_URL + 'linux/x86/gifsicle', 'linux', 'x86')
+	.src(BASE_URL + 'linux/x64/gifsicle', 'linux', 'x64')
+	.src(BASE_URL + 'freebsd/x86/gifsicle', 'freebsd', 'x86')
+	.src(BASE_URL + 'freebsd/x64/gifsicle', 'freebsd', 'x64')
+	.src(BASE_URL + 'win/x86/gifsicle.exe', 'win32', 'x86')
+	.src(BASE_URL + 'win/x86/gifsicle.exe', 'win32', 'x64')
 	.dest(path.join(__dirname, 'vendor'))
 	.use(process.platform === 'win32' ? 'gifsicle.exe' : 'gifsicle');
 
@@ -29,23 +37,23 @@ fs.exists(bin.use(), function (exists) {
 	if (!exists) {
 		bin.run(['--version'], function (err) {
 			if (err) {
-				console.log(chalk.red('✗ pre-build test failed, compiling from source...'));
+				console.log(logSymbols.warning + ' pre-build test failed, compiling from source...');
 
 				var builder = new BinBuild()
-					.src('http://www.lcdf.org/gifsicle/gifsicle-1.83.tar.gz')
+					.src('http://www.lcdf.org/gifsicle/gifsicle-' + BIN_VERSION + '.tar.gz')
 					.cfg('./configure --disable-gifview --disable-gifdiff --prefix="' + bin.dest() + '" --bindir="' + bin.dest() + '"')
 					.make('make install');
 
 				return builder.build(function (err) {
 					if (err) {
-						return console.log(chalk.red('✗ ' + err));
+						console.log(logSymbols.error, err);
 					}
 
-					console.log(chalk.green('✓ gifsicle built successfully'));
+					console.log(logSymbols.success + ' gifsicle built successfully!');
 				});
 			}
 
-			console.log(chalk.green('✓ pre-build test passed successfully'));
+			console.log(logSymbols.success + ' pre-build test passed successfully!');
 		});
 	}
 });
