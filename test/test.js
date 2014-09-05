d8453fb775bef3727fb6f41fb8806d54c09daceb
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-05 17:57:59 +0200

    Various tweaks

--- test/test.js
+++ test/test.js
@@ -4,13 +4,15 @@ var binCheck = require('bin-check');
 var BinBuild = require('bin-build');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
+var mkdir = require('mkdirp');
 var path = require('path');
+var rm = require('rimraf');
 var test = require('ava');
+var tmp = path.join(__dirname, 'tmp');
 
-test('should rebuild the gifsicle binaries', function (t) {
-	t.plan(2);
+test('rebuild the gifsicle binaries', function (t) {
+	t.plan(3);
 
-	var tmp = path.join(__dirname, 'tmp');
 	var builder = new BinBuild()
 		.src('http://www.lcdf.org/gifsicle/gifsicle-1.83.tar.gz')
 		.cmd('./configure --disable-gifview --disable-gifdiff --prefix="' + tmp + '" --bindir="' + tmp + '"')
@@ -21,11 +23,15 @@ test('should rebuild the gifsicle binaries', function (t) {
 
 		fs.exists(path.join(tmp, 'gifsicle'), function (exists) {
 			t.assert(exists);
+
+			rm(tmp, function (err) {
+				t.assert(!err);
+			});
 		});
 	});
 });
 
-test('should return path to binary and verify that it is working', function (t) {
+test('return path to binary and verify that it is working', function (t) {
 	t.plan(2);
 
 	binCheck(require('../').path, ['--version'], function (err, works) {
@@ -34,15 +40,15 @@ test('should return path to binary and verify that it is working', function (t) 
 	});
 });
 
-test('should minify a GIF', function (t) {
-	t.plan(5);
+test('minify a GIF', function (t) {
+	t.plan(6);
 
 	var args = [
-		'-o', path.join(__dirname, 'tmp/test.gif'),
+		'-o', path.join(tmp, 'test.gif'),
 		path.join(__dirname, 'fixtures/test.gif')
 	];
 
-	fs.mkdir(path.join(__dirname, 'tmp'), function (err) {
+	mkdir(tmp, function (err) {
 		t.assert(!err);
 
 		execFile(require('../').path, args, function (err) {
@@ -51,9 +57,13 @@ test('should minify a GIF', function (t) {
 			fs.stat(path.join(__dirname, 'fixtures/test.gif'), function (err, a) {
 				t.assert(!err);
 
-				fs.stat(path.join(__dirname, 'tmp/test.gif'), function (err, b) {
+				fs.stat(path.join(tmp, 'test.gif'), function (err, b) {
 					t.assert(!err);
 					t.assert(b.size < a.size);
+
+					rm(tmp, function (err) {
+						t.assert(!err);
+					});
 				});
 			});
 		});
