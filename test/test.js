dead03f122416fbfb35490896396f102e4da3502
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-12 14:23:49 +0200

    Create and remove tmp dir before and after test

--- test/test.js
+++ test/test.js
@@ -6,14 +6,12 @@ var BinBuild = require('bin-build');
 var compareSize = require('compare-size');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
-var mkdir = require('mkdirp');
 var path = require('path');
-var rm = require('rimraf');
 var test = require('ava');
 var tmp = path.join(__dirname, 'tmp');
 
 test('rebuild the gifsicle binaries', function (t) {
-	t.plan(3);
+	t.plan(2);
 
 	var cfg = [
 		'./configure --disable-gifview --disable-gifdiff',
@@ -30,10 +28,6 @@ test('rebuild the gifsicle binaries', function (t) {
 
 		fs.exists(path.join(tmp, bin.use()), function (exists) {
 			t.assert(exists);
-
-			rm(tmp, function (err) {
-				t.assert(!err, err);
-			});
 		});
 	});
 });
@@ -48,7 +42,7 @@ test('return path to binary and verify that it is working', function (t) {
 });
 
 test('minify a GIF', function (t) {
-	t.plan(5);
+	t.plan(3);
 
 	var src = path.join(__dirname, 'fixtures/test.gif');
 	var dest = path.join(tmp, 'test.gif');
@@ -57,20 +51,12 @@ test('minify a GIF', function (t) {
 		src
 	];
 
-	mkdir(tmp, function (err) {
+	execFile(require('../').path, args, function (err) {
 		t.assert(!err, err);
 
-		execFile(require('../').path, args, function (err) {
+		compareSize(src, dest, function (err, res) {
 			t.assert(!err, err);
-
-			compareSize(src, dest, function (err, res) {
-				t.assert(!err, err);
-				t.assert(res[dest] < res[src]);
-
-				rm(tmp, function (err) {
-					t.assert(!err, err);
-				});
-			});
+			t.assert(res[dest] < res[src]);
 		});
 	});
 });
