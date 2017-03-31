a903cdca732b00cde04a12b0b600135ac6af6cb7
Shogo Sensui <shogo.sensui@gmail.com>
2017-03-31 13:23:32 +0900

    ES2015ify and require Node.js 4 (#73)

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 'use strict';
-var spawn = require('child_process').spawn;
-var gifsicle = require('./');
+const spawn = require('child_process').spawn;
+const gifsicle = require('.');
 
-var input = process.argv.slice(2);
+const input = process.argv.slice(2);
 
 spawn(gifsicle, input, {stdio: 'inherit'})
 	.on('exit', process.exit);
