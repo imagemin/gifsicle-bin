7f88f03e5def54531cf6f25db53e4e2da2cd8c4b
Kevin Martensson <kevinmartensson@gmail.com>
2017-09-21 15:49:41 +0200

    Meta tweaks

--- test.js
+++ test.js
@@ -5,34 +5,31 @@ const test = require('ava');
 const execa = require('execa');
 const tempy = require('tempy');
 const binCheck = require('bin-check');
-const BinBuild = require('bin-build');
+const binBuild = require('bin-build');
 const compareSize = require('compare-size');
-const gifsicle = require('..');
+const m = require('.');
 
-test.cb('rebuild the gifsicle binaries', t => {
+test('rebuild the gifsicle binaries', async t => {
 	const tmp = tempy.directory();
 	const cfg = [
 		'./configure --disable-gifview --disable-gifdiff',
 		`--prefix="${tmp}" --bindir="${tmp}"`
 	].join(' ');
 
-	new BinBuild()
-		.src('https://github.com/kohler/gifsicle/archive/v1.88.tar.gz')
-		.cmd('autoreconf -ivf')
-		.cmd(cfg)
-		.cmd('make install')
-		.run(err => {
-			t.ifError(err);
-			t.true(fs.existsSync(path.join(tmp, 'gifsicle')));
-			t.end();
-		});
+	await binBuild.url('https://github.com/kohler/gifsicle/archive/v1.90.tar.gz', [
+		'autoreconf -ivf',
+		cfg,
+		'make install'
+	]);
+
+	t.true(fs.existsSync(path.join(tmp, 'gifsicle')));
 });
 
-test('return path to binary and verify that it is working', async t => {
-	t.true(await binCheck(gifsicle, ['--version']));
+test('verify binary', async t => {
+	t.true(await binCheck(m, ['--version']));
 });
 
-test('minify a GIF', async t => {
+test('minify a gif', async t => {
 	const tmp = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.gif');
 	const dest = path.join(tmp, 'test.gif');
@@ -41,7 +38,7 @@ test('minify a GIF', async t => {
 		src
 	];
 
-	await execa(gifsicle, args);
+	await execa(m, args);
 	const res = await compareSize(src, dest);
 
 	t.true(res[dest] < res[src]);
