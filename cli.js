b5140fc4f43c58d757e1856f7e0000286c33b13e
1000ch <shogosensui@gmail.com>
2022-05-18 10:32:24 +0900

    Fix execa import

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import process from 'node:process';
-import execa from 'execa';
+import {execa} from 'execa';
 import gifsicle from './index.js';
 
 execa(gifsicle, process.argv.slice(2), {stdio: 'inherit'});
